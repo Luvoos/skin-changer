b23850141b316bf43959691f08b40c682fdb3b54
Luvoos <simkon.debus@gmx.de>
2025-02-23 20:01:44 +0100

    modifying the plugin: centralizing skin change logic and adjusting more things

--- src/main/java/io/smcode/skinChanger/SkinChangerPlugin.java
+++ src/main/java/io/smcode/skinChanger/SkinChangerPlugin.java
@@ -5,8 +5,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkinChangerPlugin extends JavaPlugin {
 
+    public SkinManager skinManager;
+
     @Override
     public void onEnable() {
+        try {
+            init();
+        } catch (Exception e) {
+            getLogger().warning("Something went wrong: " + e.getMessage());
+        }
+        getLogger().info("SkinChanger was enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SkinChanger was disabled!");
+    }
+
+    private void init() {
+        skinManager = new SkinManager();
         getCommand("skin").setExecutor(new SkinCommand());
     }
 }
--- src/main/java/io/smcode/skinChanger/commands/SkinCommand.java
+++ src/main/java/io/smcode/skinChanger/commands/SkinCommand.java
@@ -1,78 +1,32 @@
 package io.smcode.skinChanger.commands;
 
-import com.destroystokyo.paper.profile.PlayerProfile;
-import com.destroystokyo.paper.profile.ProfileProperty;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import io.smcode.skinChanger.SkinChangerPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class SkinCommand implements CommandExecutor {
-    private static final String PROFILE_URL = "https://api.mojang.com/users/profiles/minecraft/";
-    private static final String SKIN_URL = "https://sessionserver.mojang.com/session/minecraft/profile/%s?unsigned=false";
-    private static final Map<String, Collection<ProfileProperty>> cache = new HashMap<>();
+
+    private final SkinChangerPlugin plugin = SkinChangerPlugin.getPlugin(SkinChangerPlugin.class);
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!(sender instanceof final Player player)) {
-            sender.sendMessage("§cOnly players can execute this command!");
+            sender.sendMessage("§cThis command can only be executed by players!");
             return true;
         }
 
-        if (args.length == 0) {
-            player.sendMessage("§cUsage: /skin <player>");
+        if (args.length != 1) {
+            player.sendMessage("Usage: /skin <player>");
             return true;
         }
 
-        final String targetSkin = args[0];
-        final PlayerProfile playerProfile = player.getPlayerProfile();
-        playerProfile.setProperties(getTextureProperty(targetSkin));
-        player.setPlayerProfile(playerProfile);
+        plugin.skinManager.setSkin(player, args[0], true);
 
-        player.sendMessage("§aYour skin has been changed!");
         return true;
     }
 
-    private Collection<ProfileProperty> getTextureProperty(String targetSkin) {
-        if (cache.containsKey(targetSkin))
-            return cache.get(targetSkin);
 
-        final String profileResponse = makeRequest(PROFILE_URL + targetSkin);
-        final JsonObject profileObject = JsonParser.parseString(profileResponse).getAsJsonObject();
-        final String uuid = profileObject.get("id").getAsString();
-
-        final String skinResponse = makeRequest(SKIN_URL.formatted(uuid));
-        final JsonObject skinObject = JsonParser.parseString(skinResponse).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
-        final String value = skinObject.get("value").getAsString();
-        final String signature = skinObject.get("signature").getAsString();
-        final ProfileProperty profileProperty = new ProfileProperty("textures", value, signature);
-
-        cache.put(targetSkin, List.of(profileProperty));
-
-        return List.of(profileProperty);
-    }
-
-    private String makeRequest(String url) {
-
-        try (final HttpClient client = HttpClient.newBuilder().build()) {
-            final HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-            final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
